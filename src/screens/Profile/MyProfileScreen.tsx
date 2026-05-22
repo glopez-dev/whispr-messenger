2de99291b128b5c07c39ba44683bd3dea40916e2
Roadmvn <tudy.gbaguidi@epitech.eu>
2026-05-22 11:27:15 +0200

    fix(profile): type props explicite sur les steps tour (TS7006)

--- src/screens/Profile/MyProfileScreen.tsx
+++ src/screens/Profile/MyProfileScreen.tsx
@@ -134,7 +134,7 @@ const PROFILE_TOUR_STEPS: TourStep[] = [
   {
     placement: "bottom",
     offset: 10,
-    render: (props) => (
+    render: (props: Record<string, unknown>) => (
       <TourTooltip
         {...props}
         title="Photo de profil"
@@ -146,7 +146,7 @@ const PROFILE_TOUR_STEPS: TourStep[] = [
   {
     placement: "bottom",
     offset: 10,
-    render: (props) => (
+    render: (props: Record<string, unknown>) => (
       <TourTooltip
         {...props}
         title="Modifier le profil"
